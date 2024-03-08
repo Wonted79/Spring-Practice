e37fa56b77c71024e9f826e7f9a7229c88465d94
Wonted79 <lgs000423@gmail.com>
2024-03-08 17:09:32 +0900

    2024-03-08 servlet request response 개요

---
+++ servlet/src/main/java/hello/servlet/basic/ResponseHeaderServlet.java
@@ -0,0 +1,58 @@
+package hello.servlet.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "responseHeaderServlet", urlPatterns = "/response-header")
+public class ResponseHeaderServlet extends HttpServlet {
+
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException{
+        //[status-line]
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        //[response-headers]
+        //response.setHeader("Content-Type","text/plain;charset=utf-8");
+        //cache 없애기
+        response.setHeader("Cache-Control", "no-cache,no-store,must-revalidate");
+        response.setHeader("Pragma","no-cache");
+        response.setHeader("my-header","hello");
+        
+        //[Header 편의 매서드]
+        content(response);
+        cookie(response);
+        redirect(response);
+        PrintWriter writer = response.getWriter();
+        writer.print("ok");
+        //println 으로 인해 바이트 길이가 길어질수도 있음.
+    }
+
+
+    private void content(HttpServletResponse response) {
+        //Content-Type: text/plain;charset=utf-8
+        //Content-Length: 2
+        //response.setHeader("Content-Type", "text/plain;charset=utf-8");
+        //response.setContentLength(2); //(생략시 자동 생성)
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+    }
+
+    private void cookie(HttpServletResponse response) {
+        //Set-Cookie: myCookie=good; Max-Age=600;
+        //response.setHeader("Set-Cookie", "myCookie=good; Max-Age=600");
+        Cookie cookie = new Cookie("myCookie","good");
+        cookie.setMaxAge(600);
+        response.addCookie(cookie);
+    }
+
+    private void redirect(HttpServletResponse response) {
+    }
+
+}
